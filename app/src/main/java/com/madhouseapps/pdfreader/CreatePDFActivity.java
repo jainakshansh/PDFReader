dbac25d9b29c86c98d73c19c041880f9a95ea2b7
Akshansh Jain <akshanshjn9@gmail.com>
2018-02-03 19:32:01 +0530

    Basic layout for creating PDF

---
+++ app/src/main/java/com/madhouseapps/pdfreader/CreatePDFActivity.java
@@ -0,0 +1,13 @@
+package com.madhouseapps.pdfreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatePDFActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_pdf);
+    }
+}
